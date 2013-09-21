a9750b509c4a2db7c08a4873ad412cdb2142650e
Henri Kroosmann <henri@kroosmann.ee>
2013-09-21 15:26:29 +0300

    Adding first task of second homework

---
+++ src/second/HomePage.java
@@ -0,0 +1,30 @@
+package second;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class HomePage
+ */
+public class HomePage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String attribute = (String) request.getParameter("param");
+		
+		if (attribute != null) {
+			session.setAttribute("attribute", attribute);
+		}
+		
+		response.getWriter().println("Hello");
+		response.getWriter().println("Your session id is " + session.getId());
+		response.getWriter().println("Session attribute is " + session.getAttribute("attribute"));
+	}
+
+}
